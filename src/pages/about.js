3c8ef9e257b5d3892f9bb8adc2e26b4ffa29f3d2
LayersOfAbstraction <jnash51@live.com>
2022-02-09 21:57:45 +1000

    Part 2: Create a reusable layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Step 1: Import React
 import * as React from 'react'
 import { Link } from 'gatsby'
@@ -15,20 +14,4 @@ function AboutPage() {
   )
 }
 // Step 3: Export your component
-=======
-import * as React from 'react'
-import { Link } from 'gatsby'
-
-const AboutPage = () => {
-    return (
-        <main>
-            <title>About Me</title>
-            <h1>About Me</h1>
-            <Link to="/">Back to Home</Link>
-            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-        </main>
-    )
-}
-
->>>>>>> 9e8b6092aa1637ae99afde9e6e1d769ae73b5cfb
 export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,6 @@
 // Step 1: Import React
 import * as React from "react"
-<<<<<<< HEAD
 import { Link } from "gatsby"
-=======
-import { Link } from 'gatsby'
->>>>>>> 9e8b6092aa1637ae99afde9e6e1d769ae73b5cfb
 
 // Step 2: Define your components
 function IndexPage() {
@@ -12,19 +8,11 @@ function IndexPage() {
     <main>
       <title>Home Page</title>
       <h1>Welcome</h1>
-<<<<<<< HEAD
       <Link to="/about">About</Link>
-=======
-      <Link to="/about"></Link>
->>>>>>> 9e8b6092aa1637ae99afde9e6e1d769ae73b5cfb
       <p>I'm making this by following the Gatsby Tutorial</p>
     </main>
   )
 }
 
-<<<<<<< HEAD
 //Step 3: Export your component.
 export default IndexPage
-=======
-export default IndexPage
->>>>>>> 9e8b6092aa1637ae99afde9e6e1d769ae73b5cfb
