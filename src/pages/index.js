a8cb6817a14f5f17f03a4fb62681d9458a9af43f
LayersOfAbstraction <jnash51@live.com>
2022-02-10 23:29:09 +1000

    Part 2: Style components with CSS Modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,13 @@
 // Step 1: Import React
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from 'react'
+import Layout from '../components/layout'
 
 // Step 2: Define your components
 function IndexPage() {
   return (
-    <main>
-      <title>Home Page</title>
-      <h1>Welcome</h1>
-      <Link to="/about">About</Link>
+    <Layout pageTitle={"Home Page"}>
       <p>I'm making this by following the Gatsby Tutorial</p>
-    </main>
+    </Layout>
   )
 }
 
