c00d672b4a3be6ba8b0e2558022194f9c202c3bc
LayersOfAbstraction <jnash51@live.com>
2022-02-21 22:27:10 +1000

    Part 4: Queries in page components: Create a blog page with a list of post filenames

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,36 +1,49 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
-import{
-    container,
-    heading,
-    navLinks,
-    navLinkItem,
-    navLinkText
+import { Link, useStaticQuery, graphql } from 'gatsby'
+import {
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+  siteTitle,
 } from './layout.module.css'
 
-function Layout({ pageTitle, children }) {
-    return (
-        <section className={container}>
-            <title>{pageTitle}</title>
-            <nav>
-                <ul className={navLinks}>
-                    <li className={navLinkItem}>
-                        <Link to="/" className={navLinkText}>
-                            Home
-                        </Link>
-                    </li>
-                    <li className={navLinkItem}>
-                        <Link to="/about" className={navLinkText}>
-                            About
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-            <main>
-                <h1 className={heading}>{pageTitle}</h1>
-                {children}
-            </main>
-        </section>
-    )
+const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <div className={container}>
+      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+      <nav>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>
+              Home
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  )
 }
+
 export default Layout
