1b567c8abcc4fc7e935647243e80530fa4820a70
LayersOfAbstraction <jnash51@live.com>
2022-03-22 17:03:35 +1000

    Part 5: Task: Use the MDXRenderer component to render your post’s contents in your Blog page (step 4)

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,34 +1,38 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
-
 import {graphql} from 'gatsby'
 
 // Step 2: Define your component
 const BlogPage = ({data}) => {
   return (
     <Layout pageTitle={"My Blog Posts"}>
-    <ul>
-    {
-      data.allFile.nodes.map(node => (
-        <li key={node.name}>
-          {node.name}
-        </li>
-      ))
-    }
-    </ul>
+      {        
+        data.allMdx.nodes.map(node => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+          </article>   
+        ))
+      }
     </Layout>
   )
 }
 
 export const query = graphql`
-  query {
-    allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
-      nodes {
-        name
+query MyQuery {
+  allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+    nodes {
+      frontmatter {
+        date(formatString: "MMMM D, YYYY")
+        title
       }
+      id
+      body
     }
   }
+}
+
 `
 // Step 3: Export your component
 export default BlogPage
